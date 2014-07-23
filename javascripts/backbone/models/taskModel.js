69c03ed7924bcb737896d0b275b027aac294dbc3
dshamis317 <dshamis317@gmail.com>
2014-07-23 12:33:10 -0400

    added completed attribute to task to render completed tasks

--- javascripts/backbone/models/taskModel.js
+++ javascripts/backbone/models/taskModel.js
@@ -2,6 +2,14 @@ var ToDoList = ToDoList || { Models: {}, Collections: {}, Views: {} };
 
 ToDoList.Models.Task = Backbone.Model.extend({
   defaults: {
-    name: "Relax, You've had a tough week..."
+    name: "Relax, You've had a tough week...",
+    completed: false
+  },
+  toggle: function() {
+    if (this.get('completed') === false) {
+      this.set('completed', true);
+    } else if (this.get('completed') === true ){
+      this.set('completed', false);
+    }
   }
 });
--- javascripts/backbone/views/tasks/taskView.js
+++ javascripts/backbone/views/tasks/taskView.js
@@ -10,12 +10,15 @@ ToDoList.Views.TaskView = Backbone.View.extend({
     var that = this;
     var renderedHTML = this.template({task: this.model.toJSON()});
     this.$el.html(renderedHTML);
+    if (this.model.get('completed') === true) {
+      this.$el.css('text-decoration', 'line-through')
+    }
     return this;
   },
   events: {
     'click [data-action="delete"]': 'markAsComplete'
   },
   markAsComplete: function() {
-    this.model.destroy();
+    this.model.toggle();
   }
 })
